baa65f26216f71484fd540bb4925424ec5a5c711
wheezchiz <102015448+miguelcarl@users.noreply.github.com>
2023-03-17 13:36:03 +0800

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PDC03_MODULE03
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+        private async void Activity01(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity1());
+        }
+
+        private async void Activity02(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity2());
+        }
+
+        private async void Activity03(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity3());
+        }
+
+        private async void Activity04(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new activity4());
+        }
+    }
+}
